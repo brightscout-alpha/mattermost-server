6be82bbd3650c4f7eb379063fa66d37e2a38f801
Umang Parmar <umangjparmar@gmail.com>
2020-12-10 21:28:50 +0530

    remove unnecessary const declaration

--- model/status.go
+++ model/status.go
@@ -14,7 +14,6 @@ const (
 	STATUS_AWAY            = "away"
 	STATUS_DND             = "dnd"
 	STATUS_ONLINE          = "online"
-	STATUS_CUSTOM_MESSAGE  = "custom_message"
 	STATUS_CACHE_SIZE      = SESSION_CACHE_SIZE
 	STATUS_CHANNEL_TIMEOUT = 20000  // 20 seconds
 	STATUS_MIN_UPDATE_TIME = 120000 // 2 minutes
