ba8be723f4b4fc27c0f39fd0f2711d2bf6757567
Chetanya Kandhari <chetanya.kandhari@brightscout.com>
2021-01-28 15:04:44 +0530

    MI-1145: Add custom status APIs

--- app/app_iface.go
+++ app/app_iface.go
@@ -1026,4 +1026,7 @@ type AppIface interface {
 	ViewChannel(view *model.ChannelView, userId string, currentSessionId string) (map[string]int64, *model.AppError)
 	WaitForChannelMembership(channelId string, userId string)
 	WriteFile(fr io.Reader, path string) (int64, *model.AppError)
+	SetCustomStatus(userId string, status *model.CustomStatus) *model.AppError
+	RemoveCustomStatus(userId string) *model.AppError
+	RemoveRecentCustomStatus(userId string, status *model.CustomStatus) *model.AppError
 }
